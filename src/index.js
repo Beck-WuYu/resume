f27010010871e55d553aff6a86da4bb5b7f318e4
beckwu <574196906@qq.com>
2020-07-03 10:19:31 +0800

    先搭建起架子

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const test = () => {
+    debugger;
+    const a = {
+        b: 1
+    }
+    const c = {
+        d: 2
+    }
+    console.log({...a,...c});
+}
+test();
---
+++ webpack.dev.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const fs = require('fs')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require("clean-webpack-plugin") 
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].[hash:6].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        compress: true,
+        port: 80
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                //exclude: /(node_modules|bower_components)/,//某些CSS在node_modules
+                use: ['style-loaedr', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            },
+        ]
+    }, 
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            // inject: true,
+            template: path.resolve(__dirname, 'public/index.html'),
+            minify: {}
+        })
+    ]
+   
+
+
+}
